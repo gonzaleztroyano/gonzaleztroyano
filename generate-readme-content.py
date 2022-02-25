b60e44d77da8b1be928625212223b31a460c2ea2
Pablo González <44492590+gonzaleztroyano@users.noreply.github.com>
2022-02-25 20:18:48 +0100

    Update generate-readme-content.py

--- generate-readme-content.py
+++ generate-readme-content.py
@@ -10,3 +10,4 @@ files = [f for f in os.listdir('.') if os.path.isdir(f)]
 for f in files:
     if f.startswith('.') == False:
       print (' * [',f,'](./',f,')',sep='')
+print('','Este listado ha sido generado automáticamente con [este script en Python](https://github.com/gonzaleztroyano/gonzaleztroyano/blob/main/generate-readme-content.py).','',sep='\n')
