003677546fc5701cf31aec60d0ef9d63a2437eef
Pablo González <44492590+gonzaleztroyano@users.noreply.github.com>
2022-03-29 19:14:11 +0200

    Update CF Genera-Password Worker
    Allow custom length and add license details

--- cloudflare_worker_password_generator.js
+++ cloudflare_worker_password_generator.js
@@ -1,20 +1,31 @@
 addEventListener("fetch", event => {
     event.respondWith(handleRequest(event.request))
-  })
-  
-   function genPassword() {
-      var chars = "0123456789abcdefghijklmnopqrstuvwxyz!#$%&'()*+,-./:;<=>?@[\]^_`{|}~ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-      var passwordLength = 24;
-      var password = "";
-    for (var i = 0; i <= passwordLength; i++) {
-      var randomNumber = Math.floor(Math.random() * chars.length);
-      password += chars.substring(randomNumber, randomNumber +1);
-      }
-      return password;
-      }
-  
-  async function handleRequest(request) {
-    return new Response(genPassword(), {
-      headers: { 'content-type': 'text/plain' },
+})
+
+function genPassword(pathname) {
+    var chars = "0123456789abcdefghijklmnopqrstuvwxyz!#$%&'()*+,-./:;<=>?@[\]^_`{|}~ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    if (pathname == '/') {
+        var passwordLength = 24;
+    } else {
+        pathname = parseInt(pathname.substr(1));
+        var passwordLength = pathname;
+        console.log(pathname);
+    }
+    var password = "";
+    for (var i = 0; i < passwordLength; i++) {
+        var randomNumber = Math.floor(Math.random() * chars.length);
+        password += chars.substring(randomNumber, randomNumber + 1);
+    }
+    return password;
+}
+
+async function handleRequest(request) {
+    const url = new URL(request.url);
+    return new Response(genPassword(url.pathname), {
+        headers: { 'content-type': 'text/plain' },
     })
 }
+
+// Esta obra se publica bajo la licenca Creative Commons BY 4.0 Internacional.
+// Autor y año de publicación: Pablo González, 2022
+// https://creativecommons.org/licenses/by/4.0/deed.en 
