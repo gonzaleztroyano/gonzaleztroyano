d96d367dc663729c0a82221e6c5dd57a0fe7282b
Pablo Gonzalez Troyano <pablogontroya@gmail.com>
2022-04-07 18:31:03 +0200

    Update CF Worker genera-passwd

--- cloudflare_worker_password_generator.js
+++ cloudflare_worker_password_generator.js
@@ -3,12 +3,41 @@ addEventListener("fetch", event => {
 })
 
 function genPassword(pathname) {
-    var chars = "0123456789abcdefghijklmnopqrstuvwxyz!#$%&'()*+,-./:;<=>?@[\]^_`{|}~ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let regex_num = new RegExp('^\/[0-9]+$');
+    let regex_alpha = new RegExp('^\/[a,n,A]+$');
+    let regex_alphanum = new RegExp('^\/[0-9]+\/[a,n,A]+$');
+    var chars = "0123456789abcdefghijklmnopqrstuvwxyz!#$%&()*+,-./:;<=>?@[\]^_{|}~ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     if (pathname == '/') {
         var passwordLength = 24;
-    } else {
+    } else if (regex_num.test(pathname)) {
         pathname = parseInt(pathname.substr(1));
         var passwordLength = pathname;
+    } else if (regex_alpha.test(pathname)) {
+        var passwordLength = 24;
+        pathname = pathname.substr(1);
+        if (pathname == 'a') {
+            var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        } else if (pathname == 'n') {
+            var chars = "1234567890";
+        } else if (pathname == 'A'){
+            var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        } else {
+            return "ERROR en la solicitud. Ver (ENLACE) - 1";
+        }
+    } else if (regex_alphanum.test(pathname)) {
+        let gen_opt = pathname.substr(-1);
+        var passwordLength = parseInt(pathname.substr(1).slice(0, -2));
+        if (gen_opt == 'a') {
+            var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        } else if (gen_opt == 'n') {
+            var chars = "1234567890";
+        } else if (gen_opt == 'A'){
+            var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        } else {
+            return "ERROR en la solicitud. Ver (ENLACE) - 2";
+        }
+    } else {
+        return "ERROR en la solicitud. Ver (ENLACE) - 3";
     }
     var password = "";
     for (var i = 0; i < passwordLength; i++) {
